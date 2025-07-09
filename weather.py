58ddefcb8419b86f7765c7c3ec1dadc19983640c
aneeshamanke <aneeshamanke@gmail.com>
2025-07-09 23:56:27 +0530

    minor changes

--- weather.py
+++ weather.py
@@ -1,24 +1,22 @@
 import requests
 import os
+from dotenv import load_dotenv
 
-WEATHER_API_KEY=os.getenv("WEATHER_API_KEY")
+load_dotenv()
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
 def get_weather(city):
-    url=f"http://api.openweathermap.org/data/2.5/weather"
-    params={
-        "q":city,
-        "aapid":WEATHER_API_KEY,
-        "units":"metric"
+    url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": WEATHER_API_KEY,
+        "units": "metric"
     }
-    
-    response=requests.get(url,params=params)
-    if response.status_code==200:
-        data=response.json()
-        desc=data['weather'][0]['description'].capitalize()
-        temp=data['main']['temp']
-        
-        return f"{city.title()}:{desc},{temp}c"
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        desc = data['weather'][0]['description'].capitalize()
+        temp = data['main']['temp']
+        return f"{city.title()}: {desc}, {temp}°C"
     else:
-        return f"weather for the {city} not found"
-    
-    
+        return f"Weather for {city} not found."
